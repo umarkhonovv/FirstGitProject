8e3a56d4014e370fe2cf85b641858a2644fac53f
umarkhonovv <umarhonovasqar699@gmail.com>
2024-11-07 11:51:44 +0500

    Code changed

--- src/Program.cs
+++ src/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Enter a 4-digit number :");
+            var number = Convert.ToInt32(Console.ReadLine());
+            var ones = number % 10;
+            var tens = number / 10 % 10;
+            var hundreds = number / 100 % 10;
+            var thousands = number / 1000 % 10;
+            var result = ones + tens + hundreds + thousands;
+            Console.WriteLine(result);
+
         }
     }
 }
